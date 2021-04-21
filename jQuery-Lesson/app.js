e4fc75161b3f3e09f872618fd15676a8c0bdbd90
yoshiki0756 <g18lb014.yoshiki@gmail.com>
2021-04-21 16:45:13 +0900

    [Finish]6章

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,9 +1,8 @@
 $(function(){
-  $('.box1').slideDown(function(){
-    $('.box1').css({
-      'background-color': '#0000FF',
-      'height': '100px',
-      'width': '200px'
-    }).slideUp();
+  $('.box1').on('click',function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
   });
 });
